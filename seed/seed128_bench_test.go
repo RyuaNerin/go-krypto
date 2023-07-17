92e44a33a8eac2b06abf388e1748e6b2f53cfdb9
RyuaNerin <admin@ryuar.in>
2023-07-18 01:32:51 +0900

    add SEED testcases

--- seed/seed128_bench_test.go
+++ seed/seed128_bench_test.go
@@ -8,7 +8,7 @@ import (
 
 var (
 	testKey128 = internal.HB(`00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00`)
-	input      = internal.HB(`00 01 02 03 04 05 06 07 08 09 0A 0B 0C 0D 0E 0F`)
+	input      = internal.HB(`00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00`)
 )
 
 func BenchmarkNewCipher128(b *testing.B) {
