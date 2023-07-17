59f59f21ccb9dbaacd152f41cdf3ecc39412b6c4
RyuaNerin <admin@ryuar.in>
2023-07-18 01:30:40 +0900

    add HIGHT testcases

--- hight/hight_bench_test.go
+++ hight/hight_bench_test.go
@@ -7,7 +7,7 @@ import (
 )
 
 var (
-	testKey = internal.Reverse(internal.HB(`00 11 22 33 44 55 66 77 88 99 aa bb cc dd ee ff`))
+	testKey = internal.Reverse(internal.HB(`00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00`))
 	input   = internal.Reverse(internal.HB(`00 00 00 00 00 00 00 00`))
 )
 
