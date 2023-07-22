11246eefff25339d2798ca8354c88747e39a4749
RyuaNerin <admin@ryuar.in>
2023-07-23 00:49:45 +0900

    update dev_test

--- lsh256/test_dev_test.go
+++ lsh256/test_dev_test.go
@@ -4,23 +4,20 @@ package lsh256
 
 import (
 	"bytes"
-	"log"
 	"testing"
 )
 
 var (
 	input = []byte{0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14}
 	a224  = []byte{22, 120, 202, 175, 245, 115, 11, 8, 161, 36, 224, 164, 169, 146, 50, 189, 139, 212, 48, 102, 89, 55, 192, 53, 148, 136, 101, 121, 3, 245, 226, 105}
-	a256  = []byte{24, 235, 63, 13, 177, 177, 2, 51, 177, 247, 172, 35, 8, 10, 194, 85, 240, 18, 233, 33, 40, 20, 177, 182, 223, 125, 148, 192, 148, 189, 166, 255}
+	a256  = []byte{230, 166, 58, 163, 40, 218, 17, 142, 7, 60, 35, 188, 131, 19, 32, 171, 175, 219, 140, 251, 19, 16, 97, 75, 94, 75, 66, 111, 48, 81, 68, 85}
 )
 
 func Test_LSH224_SSE2_DEV(t *testing.T) { testAsmDev(t, lshType256H224, a224, simdSetSSE2) }
 func Test_LSH256_SSE2_DEV(t *testing.T) { testAsmDev(t, lshType256H256, a256, simdSetSSE2) }
 
-/**
-func Test_LSH224_SSSE3(t *testing.T) { testAsm(t, lshType256H224, simdSetSSSE3) }
-func Test_LSH256_SSSE3(t *testing.T) { testAsm(t, lshType256H256, simdSetSSSE3) }
-*/
+func Test_LSH224_SSSE3_DEV(t *testing.T) { testAsmDev(t, lshType256H224, a224, simdSetSSSE3) }
+func Test_LSH256_SSSE3_DEV(t *testing.T) { testAsmDev(t, lshType256H256, a256, simdSetSSSE3) }
 
 func Test_LSH224_AVX2_DEV(t *testing.T) { testAsmDev(t, lshType256H224, a224, simdSetAVX2) }
 func Test_LSH256_AVX2_DEV(t *testing.T) { testAsmDev(t, lshType256H256, a256, simdSetAVX2) }
@@ -37,7 +34,6 @@ func testAsmDev(t *testing.T, algType algType, answer []byte, s simdSet) {
 	s.update(&ctx, input, uint32(len(input)*8))
 	s.final(&ctx, dst)
 
-	log.Println(dst)
 	if !bytes.Equal(answer, dst) {
 		t.Fail()
 	}
